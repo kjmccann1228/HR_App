d784f6448ba9872bf210a9ae17f47b7d11a65934
Kurt McCann <kurtmccann@MacBook-Pro-20.local>
2016-11-02 17:32:38 -0400

    updated

--- app/scripts/controllers/viewUserCtrl.js
+++ app/scripts/controllers/viewUserCtrl.js
@@ -9,58 +9,33 @@
  */
 
 angular.module('hrApp40App')
-  .controller('ViewUserCtrl', ['$scope', '$http'])
-  // function ($scope, $http){
-    $scope.responses = [];
-    var person =
+  .controller('ViewUserCtrl', ['$scope', '$http',
+  function ($scope, $http){
+
+    function successCallback(response)
     {
-      "lastName": "Valenta",
-      "firstName": "Zach",
-      "payRate": 10.00
-    }
+      console.log(response.data);
+       $scope.responses = response.data._embedded.users;
+     }
+
+     function failCallback(response)
+     {
+       console.log("AAAAAAAAHHHHHHHHH!!!!!!");
+       console.error(response.data);
+
+      }
+
+    $http.get("http://localhost:8080/hrusers/search/findByLastName?lastName=Momma")
+    .then(successCallback, failCallback)
+      }
+
+      // $http.get("http://localhost:8080/hrusers/search/findByLastName?lastName=Red")
 
 
-    // $http.get("localhost:8080/hrusers/search/findByLastName?lastName=Momma")
+
+    ]);
+
     // .then(
-    //   function(response){
-    //     console.log(response.data);
-        //  $scope.responses = response.data;
-    //   });
-    $scope.responses = person;
-;
-
-
-//
-// 'use strict';
-//
-// /**
-//  * @ngdoc function
-//  * @name hrClientApp.controller:AboutCtrl
-//  * @description
-//  * # AboutCtrl
-//  * Controller of the hrClientApp
-//  */
-// angular.module('hrClientApp')
-//   .controller('AboutCtrl', ['$scope', '$http',
-//   function ($scope, $http){
-//     $scope.responses = [];
-//     $http.get("http://localhost:8080/timecard")
-//     .then(
-//       function(response){
-//         console.log(response.data);
-//         $scope.responses = response.data;
-//       });
-//   $scope.delete = function(){
-//
-//      var idSpot = prompt("Please enter an ID of the timeCard you want to delete")
-//
-//           var id  = [{
-//             'id': idSpot
-//           }];
-//           console.log(id.id);
-//           $http.delete("http://localhost:8080/timeCardId?id="+id[0].id);
-//
-//     };
-//
-//
-// }]);
+    // function successCallback(response){
+    //   console.log(response.data);
+    //    $scope.responses = response.data._embedded.users;
